4b3323a23597c81e7d1bf43f93597a619abfe950
fuhad479 <fuhadhasan479@gmail.com>
2023-02-01 16:25:49 +0600

    completed header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,33 @@
-import TopBar from '../TopBar'
+import { ReactComponent as Logo } from "src/assets/logo.svg";
 
-import './index.scss'
+import TopBar from "../TopBar";
+
+import "./index.scss";
 
 export default function Header() {
-    return (
-        <header className="header">
-            <TopBar />
-            <div className="main-header">Main Header</div>
-        </header>
-    )
+  return (
+    <header className="header">
+      <TopBar />
+      <div className="main-header">
+        <div className="container">
+          <div className="main-header-content">
+            <div className="logo">
+              <Logo />
+            </div>
+            <nav className="nav">
+              <ul className="navigation">
+                <li className="navigation-item">Company</li>
+                <li className="navigation-item">Services</li>
+                <li className="navigation-item">Hire Developers</li>
+                <li className="navigation-item">Case Study</li>
+                <li className="navigation-item">Resources</li>
+                <li className="navigation-item">Contact Us</li>
+                <button type="button" className="large-button">Get A Free Quote</button>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
